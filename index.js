210c1e782beb11c40267e94d7df5c7aa5eea8d0c
Glitch (mittens) <none>
2020-09-30 16:30:26 +0000

    👨⛄️ Updated with Glitch

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 
 
-// Register service worker to control making site work offline
-if('serviceWorker' in navigator) {
-  navigator.serviceWorker
-           .register('/sw.js')
-           .then(function() { console.log('Service Worker Registered'); });
-}
+
 
 // Code to handle install prompt on desktop
 
 let deferredPrompt;
 const addBtn = document.querySelector('.add-button');
-//addBtn.style.display = 'none';
+addBtn.style.display = 'none';
 
 window.addEventListener('beforeinstallprompt', (e) => {
   // Prevent Chrome 67 and earlier from automatically showing the prompt
