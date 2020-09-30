247672079b02e057c89e0d7b83b90968f7d04885
Glitch (mittens) <none>
2020-09-30 14:04:27 +0000

    👽🌸 Updated with Glitch

--- assets/pwa/sw.js
+++ assets/pwa/sw.js
@@ -2,14 +2,7 @@ self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('fox-store').then(function(cache) {
      return cache.addAll([
-       'https://mdn.github.io/pwa-examples/a2hs/',
-       'https://mdn.github.io/pwa-examples/a2hs/index.html',
-       'https://mdn.github.io/pwa-examples/a2hs/index.js',
-       'https://mdn.github.io/pwa-examples/a2hs/style.css',
-       'https://mdn.github.io/pwa-examples/a2hs/images/fox1.jpg',
-       'https://mdn.github.io/pwa-examples/a2hs/images/fox2.jpg',
-       'https://mdn.github.io/pwa-examples/a2hs/images/fox3.jpg',
-       'https://mdn.github.io/pwa-examples/a2hs/images/fox4.jpg'
+       '/',
      ]);
    })
  );
