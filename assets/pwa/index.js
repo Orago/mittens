a261d719535bb1c7515ab19aff356770a4428f03
Glitch (mittens) <none>
2020-09-30 13:37:49 +0000

    ☕️☺️ Updated with Glitch

--- assets/pwa/index.js
+++ assets/pwa/index.js
@@ -1,3 +1,9 @@
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker
+           .register('/assets/pwa/sw.js')
+           .then(function() { console.log('Service Worker Registered'); });
+}
+
 let deferredPrompt;
 const addBtn = document.querySelector('.add-button');
 addBtn.style.display = 'none';
